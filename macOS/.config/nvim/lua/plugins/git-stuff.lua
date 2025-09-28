a7bcc940db1b5b2ff0c69a84f213cc69716e2164
Pau Matas <pmatasalbiol@gmail.com>
2025-09-28 15:05:25 +0200

    nvim git changes

--- macOS/.config/nvim/lua/plugins/git-stuff.lua
+++ macOS/.config/nvim/lua/plugins/git-stuff.lua
@@ -8,7 +8,8 @@ return {
       require("gitsigns").setup()
 
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+      vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
+      vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk<CR>", {})
     end
   }
 }
