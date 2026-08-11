98355d37eee5b46a0433f8fefba6f82c34f4b9f2
Pau Matas <pau.matas@happyrobot.ai>
2026-08-11 09:17:37 +0200

    feat(zsh): accept suggestions word by word

--- macOS/.config/zsh/autosuggestions.zsh
+++ macOS/.config/zsh/autosuggestions.zsh
@@ -1,3 +1,6 @@
 # Inline suggestions from shell history
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# Accept the next word of a suggestion with Option+Right
+bindkey '^[[1;3C' forward-word
